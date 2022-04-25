ac3786c668029b7eb8aa0f9277236ff902ac5a31
samphoto <samidarnaud@gmail.com>
2022-04-25 18:30:56 +0200

    build(init-contract): set up foundations of the architecture

---
+++ src/PocketFaucet.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract PocketFaucet {
+    using SafeERC20 for IERC20;
+}
