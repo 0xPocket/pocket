b32e0eb73e49b2c269498061e4a5d7ac6af576d3
samphoto <samidarnaud@gmail.com>
2022-04-26 18:23:50 +0200

    feat(withdraw-in-contract): withdraw now working with unit test done
    BREAKING CHANGE:

--- src/PocketFaucet.sol
+++ src/PocketFaucet.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/console.sol";
+import "forge-std/console2.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract PocketFaucet {
+// TO DO : check multisig
+// TO DO : gouvernor should be -> TimelockController
+// TO DO : test roles
+
+contract PocketFaucet is AccessControl {
     using SafeERC20 for IERC20;
+
+    bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+    bytes32 public constant PARENT_ROLE = keccak256("PARENT_ROLE");
+    bytes32 public constant CHILD_ROLE = keccak256("CHILD_ROLE");
 
     address immutable baseToken;
     uint256 timestamp;
@@ -17,6 +26,7 @@ contract PocketFaucet {
     constructor(uint256 begin, address token) {
         baseToken = token;
         timestamp = begin;
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     struct config {
@@ -27,6 +37,7 @@ contract PocketFaucet {
         bytes32[] parents;
     }
 
+    // TO DO : test update
     function updateTimestamp() public {
         if (timestamp + 1 weeks < block.timestamp) timestamp += 1 weeks;
     }
@@ -34,5 +45,18 @@ contract PocketFaucet {
     // gestion des parents => initialisation config, change config, add money...
 
     // gestion de l'enfant => claim, change address...
-    // gestion de l'argent de pocket => withdraw, transfert...
+
+    function withdrawToken(address token, uint256 amount)
+        public
+        onlyRole(WITHDRAW_ROLE)
+    {
+        IERC20(token).safeTransfer(msg.sender, amount);
+    }
+
+    function withdrawCoin(uint256 amount) public onlyRole(WITHDRAW_ROLE) {
+        if (amount == 0) amount = address(this).balance;
+        payable(msg.sender).transfer(amount);
+    }
+
+    receive() external payable {}
 }
