701c07c5ab24f535db2b92df410f2cdc6b8756ef
samphoto <samidarnaud@gmail.com>
2022-04-26 13:19:34 +0200

    feat(SC-struct): draw the beginning of SC faucet

--- src/PocketFaucet.sol
+++ src/PocketFaucet.sol
@@ -7,4 +7,32 @@ import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract PocketFaucet {
     using SafeERC20 for IERC20;
+
+    address immutable baseToken;
+    uint256 timestamp;
+
+    mapping(bytes32 => uint256) public parentBalance;
+    mapping(bytes32 => address[]) public parentToChildren;
+
+    constructor(uint256 begin, address token) {
+        baseToken = token;
+        timestamp = begin;
+    }
+
+    struct config {
+        uint256 ceiling;
+        uint256 claimable;
+        bool active;
+        uint256 lastPeriod;
+        bytes32[] parents;
+    }
+
+    function updateTimestamp() public {
+        if (timestamp + 1 weeks < block.timestamp) timestamp += 1 weeks;
+    }
+
+    // gestion des parents => initialisation config, change config, add money...
+
+    // gestion de l'enfant => claim, change address...
+    // gestion de l'argent de pocket => withdraw, transfert...
 }
