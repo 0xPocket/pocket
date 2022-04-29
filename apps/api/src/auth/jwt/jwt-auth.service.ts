ec73968f855db2ef0c3c639c41725e0e2e2e773a
Solal Dunckel <solaldunckel@gmail.com>
2022-04-29 10:53:06 +0000

    feat(api): add new jwt token generation for authentication and child signup flow

--- apps/api/src/auth/jwt/jwt-auth.service.ts
+++ apps/api/src/auth/jwt/jwt-auth.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { JwtChildSignupTokenPayload } from './dto/JwtChildSignupTokenPayload.dto';
+import { JwtEmailTokenPayload } from './dto/JwtEmailTokenPayload.dto';
 import { JwtTokenPayload } from './dto/JwtTokenPayload.dto';
 
 @Injectable()
 export class JwtAuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
-  generateUserToken(userId: string, isParent = true) {
+  generateAuthenticationToken(userId: string, isParent = true) {
     const payload: JwtTokenPayload = {
       userId: userId,
       isParent,
     };
     return this.jwtService.sign(payload);
+  }
+
+  generateEmailConfirmationToken(email: string) {
+    const payload: JwtEmailTokenPayload = {
+      email,
+    };
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_EMAIL_SECRET'),
+      expiresIn: '1h',
+    });
+  }
+
+  generateChildSignupToken(userId: string) {
+    const payload: JwtChildSignupTokenPayload = {
+      userId,
+    };
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_CHILD_SECRET'),
+      expiresIn: '12h',
+    });
   }
 }
