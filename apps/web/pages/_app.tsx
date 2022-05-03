7f12d0cab0379a86d22b9dfc79f553e8672e8c5f
Solal Dunckel <solaldunckel@gmail.com>
2022-05-03 07:07:21 +0000

    feat(web): implement web3-react for front-end metamask auth

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,12 +2,22 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from '@lib/nest-auth/next';
 import config from '../next-auth.config';
 import '../styles/globals.css';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider: any) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
 
 function App({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
-    <AuthProvider config={config}>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <AuthProvider config={config}>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </Web3ReactProvider>
   );
 }
 
