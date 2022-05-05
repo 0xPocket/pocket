9312f9b8f437ebd20de1ec55a1aa45caf776b5a8
Solal Dunckel <solaldunckel@gmail.com>
2022-05-05 12:50:44 +0000

    feat(web-child): migrate web3 module from web3-react to web3modal + add hooks for connection

---
+++ apps/web-child/contexts/contract.tsx
@@ -0,0 +1,65 @@
+import { Web3Provider } from '@ethersproject/providers';
+import axios from 'axios';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Web3Modal from 'web3modal';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import test from '@lib/contract/abi.json';
+import { Contract, ethers } from 'ethers';
+import { useWeb3Auth } from './web3hook';
+
+interface SmartContractProviderProps {
+  children: React.ReactNode;
+}
+
+interface ISmartContractContext {
+  active: boolean;
+}
+
+export function createCtx<A extends {} | null>() {
+  const ctx = createContext<A | undefined>(undefined);
+  return [ctx, ctx.Provider] as const; // 'as const' makes TypeScript infer a tuple
+}
+
+const [SmartContractContext, SmartContractContextProvider] =
+  createCtx<ISmartContractContext>();
+
+export const SmartContractProvider = ({
+  children,
+}: SmartContractProviderProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { address, provider } = useWeb3Auth();
+  const [contract, setContract] = useState<Contract>();
+
+  // console.log(abi);
+  useEffect(() => {
+    const newContract = new Contract(
+      '0x62a4b53a1de480be4bdb9c10d0f7de69aeb30abd',
+      test.abi,
+    );
+    setContract(newContract);
+    console.log('new contract set');
+  }, [provider]);
+
+  return (
+    <SmartContractContextProvider
+      value={{
+        active: true,
+      }}
+    >
+      {children}
+    </SmartContractContextProvider>
+  );
+};
+
+export function useSmartContract() {
+  const c = useContext<ISmartContractContext | undefined>(SmartContractContext);
+  if (c === undefined)
+    throw new Error('useCtx must be inside a Provider with a value');
+  return c;
+}
