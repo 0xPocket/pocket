25f31a93cc58fab88bc8f048b8bcf2baf83968ee
Solal Dunckel <solaldunckel@gmail.com>
2022-05-07 13:19:33 +0000

    chore(eslint): add typescript linting for next.js apps

--- packages/config/eslint-next.js
+++ packages/config/eslint-next.js
@@ -1,12 +1,22 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: ["next", "prettier", "plugin:@typescript-eslint/recommended"],
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
     },
   },
   rules: {
+    "@typescript-eslint/no-var-requires": "off",
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "@typescript-eslint/ban-types": [
+      "warn",
+      {
+        types: {
+          "{}": false,
+        },
+      },
+    ],
   },
 };
