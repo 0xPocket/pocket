c3b9ff01a97c83c8c205ed1aac0300ed24384870
tipii <theo.palhol@gmail.com>
2022-05-12 10:42:10 +0000

    feat(wallet tabs): tab mechanism in wallet, add fontawesome

--- apps/web-parent/components/wallet/WalletPopover.tsx
+++ apps/web-parent/components/wallet/WalletPopover.tsx
@@ -1,20 +1,13 @@
 import { Popover } from '@headlessui/react';
-import { useAuth } from '@lib/nest-auth/next';
-import { UserParent } from '@lib/types/interfaces';
-import { BigNumber } from 'ethers';
 import { useState } from 'react';
-import { useQuery } from 'react-query';
-import { useSmartContract } from '../../contexts/contract';
-import Button from '../common/Button';
 import { usePopper } from 'react-popper';
-import { toast } from 'react-toastify';
-import { roundBigNumbertoString } from '../../utils/reactQuery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+import WalletContent from './WalletContent';
 
 type WalletPopoverProps = {};
 
 function WalletPopover({}: WalletPopoverProps) {
-  const { user } = useAuth<UserParent>();
-  const { provider } = useSmartContract();
   const [referenceElement, setReferenceElement] =
     useState<HTMLButtonElement | null>(null);
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
@@ -24,17 +17,11 @@ function WalletPopover({}: WalletPopoverProps) {
     placement: 'bottom-end',
   });
 
-  const balanceQuery = useQuery(
-    'balance',
-    async () => await provider?.getBalance(user?.wallet.publicKey!),
-    {
-      select: (data: BigNumber | undefined) => roundBigNumbertoString(data, 2),
-    },
-  );
-
   return (
     <Popover className="relative">
-      <Popover.Button ref={setReferenceElement}>Wallet</Popover.Button>
+      <Popover.Button ref={setReferenceElement}>
+        <FontAwesomeIcon icon={faWallet} />
+      </Popover.Button>
       <Popover.Overlay className="fixed inset-0 z-20 bg-dark opacity-30" />
       <Popover.Panel
         ref={setPopperElement}
@@ -42,34 +29,7 @@ function WalletPopover({}: WalletPopoverProps) {
         style={styles.popper}
         {...attributes.popper}
       >
-        <div className="flex min-w-[400px] flex-col gap-4 rounded-lg bg-bright px-8 py-4 shadow-lg">
-          <div className="flex items-center justify-between border-b pb-4">
-            <h2 className="">My Wallet</h2>
-            <Button
-              action={() => {
-                navigator.clipboard.writeText(user?.wallet.publicKey!);
-                toast.success('Address copied to clipboard !');
-              }}
-            >
-              Copy address
-            </Button>
-          </div>
-          <div className="border-b pb-4">
-            <span>Available funds</span>
-            <div>
-              {balanceQuery.isLoading ? (
-                <h2>loading...</h2>
-              ) : (
-                <div className="flex items-end gap-2">
-                  <h2>{balanceQuery.data}</h2>
-                  <span>MATIC</span>
-                </div>
-              )}
-            </div>
-          </div>
-          <Button>Top-Up</Button>
-          <Button light>Settings</Button>
-        </div>
+        <WalletContent />
       </Popover.Panel>
     </Popover>
   );
