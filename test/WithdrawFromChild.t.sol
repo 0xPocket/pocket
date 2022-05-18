cebbfdf3aef72ef8e25bce8b3407e39363db3c47
gdupont <gdupont.pro@gmail.com>
2022-05-18 17:53:55 +0200

    test(withdrawfundsfromchild): add 2 tests
    withdraw more and withdraw partial amount

--- test/WithdrawFromChild.t.sol
+++ test/WithdrawFromChild.t.sol
@@ -14,17 +14,34 @@ contract WithdrawFromChild is Test, PFHelper {
         addFundToChild(parent1, 100e18, child1);
     }
 
-    function testWithdraw() public {
+    function testWithdrawAll() public {
         vm.startPrank(parent1);
         uint256 amountBefore = checkBalance(baseTokenHelper, parent1);
         PF.withdrawFundsFromChild(0, child1);
         uint256 amountAfter = checkBalance(baseTokenHelper, parent1);
         assertEq(amountAfter, amountBefore + 100e18);
+        assertEq(getConfig(child1).balance, 0);
     }
 
     function testWithdrawWrongChild() public {
         vm.startPrank(parent2);
         vm.expectRevert(bytes("!_areRelated : child doesn't match"));
         PF.withdrawFundsFromChild(0, child1);
+    }
+
+    function testWithdrawPartial() public {
+        vm.startPrank(parent1);
+        uint256 toWithdraw = 50e18;
+        uint256 amountBefore = checkBalance(baseTokenHelper, parent1);
+        PF.withdrawFundsFromChild(toWithdraw, child1);
+        uint256 amountAfter = checkBalance(baseTokenHelper, parent1);
+        assertEq(amountAfter, amountBefore + 100e18 - toWithdraw);
+        assertEq(getConfig(child1).balance, 100e18 - toWithdraw);
+    }
+
+    function testWithdrawMore() public {
+        vm.startPrank(parent1);
+        vm.expectRevert(bytes("!withdrawFundsFromChild: amount > childBalance"));
+        PF.withdrawFundsFromChild(110e18, child1);
     }
 }
