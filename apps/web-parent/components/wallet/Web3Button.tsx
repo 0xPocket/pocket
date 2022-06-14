69407ac049bd320aaf0e872b8e134bd669ded49e
tipii <theo.palhol@gmail.com>
2022-06-14 11:06:05 +0000

    fix(web3button): introduce asDiv props

--- apps/web-parent/components/wallet/Web3Button.tsx
+++ apps/web-parent/components/wallet/Web3Button.tsx
@@ -1,5 +1,5 @@
 import { ParentContract } from 'pocket-contract/ts';
-import { FormInputText } from '@lib/ui';
+import { Button, DialogPopupWrapper } from '@lib/ui';
 import { Wallet } from 'ethers';
 import { MouseEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -12,6 +12,7 @@ type Web3ButtonProps = {
   callback?: (signer: Wallet) => void;
   contract?: (contract: ParentContract) => void;
   type?: 'button' | 'submit' | 'reset' | undefined;
+  asDiv?: boolean;
 };
 
 interface FormValues {
@@ -23,23 +24,20 @@ function Web3Button({
   callback,
   contract,
   type = 'button',
+  asDiv = false,
 }: Web3ButtonProps) {
   const { wallet } = useWallet();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
   const [showModal, setShowModal] = useState(false);
+  const [password, setPassword] = useState('');
 
   const onClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setShowModal(true);
   };
 
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = () => {
     try {
-      const signer = getSigner(wallet?.encryptedPrivateKey!, data.password);
+      const signer = getSigner(wallet?.encryptedPrivateKey!, password);
 
       if (callback) {
         callback(signer);
@@ -57,6 +55,41 @@ function Web3Button({
     setShowModal(false);
   };
 
+  if (asDiv)
+    return (
+      <>
+        <button
+          onClick={onClick}
+          type={type}
+          className="flex items-center justify-center overflow-hidden whitespace-nowrap rounded-md bg-primary px-4 py-3 text-bright dark:bg-primary-dark"
+        >
+          {children}
+        </button>
+        {showModal && (
+          <div className="absolute inset-0 flow-root">
+            <div className="fixed inset-0 z-10 flex items-center justify-center p-4">
+              <div className="relative flex flex-col gap-4 rounded-md bg-dark p-4">
+                <div
+                  className="absolute right-2 top-2 cursor-pointer"
+                  onClick={() => setShowModal(false)}
+                >
+                  X
+                </div>
+                <h2 className="">Unlock Wallet</h2>
+                <input
+                  className="w-full rounded-md border border-gray-light px-5 py-3 placeholder-gray focus:border-primary-dark focus:ring-primary-dark dark:text-gray sm:max-w-xs"
+                  placeholder="Password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button action={() => onSubmit()}>Submit</Button>
+              </div>
+            </div>
+          </div>
+        )}
+      </>
+    );
+
   return (
     <>
       <button
@@ -66,36 +99,19 @@ function Web3Button({
       >
         {children}
       </button>
-      {showModal && (
-        <div className="absolute inset-0 flow-root">
-          <div className="fixed inset-0 z-10 flex items-center justify-center p-4">
-            <div className="mx-auto rounded-md bg-dark p-4">
-              <div className="flex items-center gap-4 pb-4 ">
-                <h2 className="">Unlock Wallet</h2>
-              </div>
-              <form
-                className="flex flex-col gap-4"
-                onSubmit={handleSubmit(onSubmit)}
-              >
-                <FormInputText
-                  type="password"
-                  placeHolder="Password"
-                  registerValues={register('password', {
-                    required: 'This field is required',
-                  })}
-                  error={errors.password}
-                />
 
-                <input
-                  type="submit"
-                  value="Submit"
-                  className="rounded-md bg-dark  px-4 py-3 text-bright"
-                />
-              </form>
-            </div>
-          </div>
+      <DialogPopupWrapper isOpen={showModal} setIsOpen={setShowModal}>
+        <div className="flex flex-col gap-4 rounded-md bg-dark p-4">
+          <h2 className="">Unlock Wallet</h2>
+          <input
+            className="w-full rounded-md border border-gray-light px-5 py-3 placeholder-gray focus:border-primary-dark focus:ring-primary-dark dark:text-gray sm:max-w-xs"
+            placeholder="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button action={() => onSubmit()}>Submit</Button>
         </div>
-      )}
+      </DialogPopupWrapper>
     </>
   );
 }
