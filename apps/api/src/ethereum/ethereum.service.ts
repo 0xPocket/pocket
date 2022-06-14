412678b4a64d29ff49d1e2f2c528ea955c7ded77
Solal Dunckel <solaldunckel@gmail.com>
2022-06-14 16:39:19 +0200

    fix(api): remove import from pocket-contract temporarly

--- apps/api/src/ethereum/ethereum.service.ts
+++ apps/api/src/ethereum/ethereum.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { providers, utils } from 'ethers';
+import { Contract, providers } from 'ethers';
 import { ParentsService } from 'src/users/parents/parents.service';
 import {
   SendTransactionDto,
   TransactionType,
 } from './dto/send-transaction.dto';
+import * as PocketFaucet from 'pocket-contract/artifacts/contracts/PocketFaucet.sol/PocketFaucet.json';
 
-import {
-  PocketFaucet,
-  PocketFaucet__factory,
-} from 'pocket-contract/typechain-types';
+// import {
+//   PocketFaucet,
+//   PocketFaucet__factory,
+// } from 'pocket-contract/typechain-types';
+
 @Injectable()
 export class EthereumService {
   provider: providers.JsonRpcProvider;
-  contract: PocketFaucet;
+  // contract: PocketFaucet;
 
   constructor(private parentsService: ParentsService) {
     this.provider = new providers.JsonRpcProvider('http://localhost:8545');
-    this.contract = PocketFaucet__factory.connect(
+
+    // this.contract = PocketFaucet__factory.connect(
+    //   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    //   this.provider,
+    // );
+
+    const contract = new Contract(
       process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+      PocketFaucet.abi as any,
       this.provider,
     );
 
-    this.contract.on(
-      this.contract.filters['childAdded(address,address)'](),
+    contract.on(
+      contract.filters['childAdded(address,address)'](),
       (parentAddress, childAddress) => {
         this.parentsService.validateChildren(childAddress.toLowerCase());
         // console.log(parentAddress);
