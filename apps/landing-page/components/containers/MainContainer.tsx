a6236c32a2b50b29d4dd36249244416af2b51586
tipii <theo.palhol@gmail.com>
2022-06-24 13:27:09 +0000

    fix(landing page): fix style

--- apps/landing-page/components/containers/MainContainer.tsx
+++ apps/landing-page/components/containers/MainContainer.tsx
@@ -5,7 +5,7 @@ type MainContainerProps = { children: React.ReactNode };
 
 function MainContainer({ children }: MainContainerProps) {
   return (
-    <div className=" container relative mx-auto min-h-screen w-screen  overflow-hidden">
+    <div className=" container relative mx-auto min-h-screen w-screen">
       <Header />
       {children}
     </div>
