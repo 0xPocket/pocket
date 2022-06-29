c4502198815dbe2ca480b9ff3cc645b2239e7a83
tipii <theo.palhol@gmail.com>
2022-06-29 09:34:08 +0000

    style(landing page survey): fix responsivness

--- apps/landing-page/components/form/Question.tsx
+++ apps/landing-page/components/form/Question.tsx
@@ -10,11 +10,13 @@ type Props = {
 const Question: React.FC<Props> = ({ register, header, options }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
-      <h1 className="max-w-3xl leading-normal">{header}</h1>
-      <div className="flex gap-8 text-xl">
+      <h1 className="max-w-3xl text-center text-3xl leading-normal md:text-4xl">
+        {header}
+      </h1>
+      <div className="flex flex-col gap-8 md:flex-row md:text-xl">
         {options.map((option, index) => (
           <div
-            className=" relative min-w-[140px] rounded-lg bg-primary p-4"
+            className="relative min-w-[140px] rounded-lg bg-primary p-4"
             key={index}
           >
             <label className="flex items-center justify-center">{option}</label>
