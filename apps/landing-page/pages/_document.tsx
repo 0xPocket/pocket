967ac24aab400dc05427e1eeb5424129218888c1
Solal Dunckel <solaldunckel@gmail.com>
2022-07-15 14:06:17 +0200

    feat(landing-page): add Google Analytics

--- apps/landing-page/pages/_document.tsx
+++ apps/landing-page/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -18,6 +19,20 @@ export default function Document() {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=UA-234633944-1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-234633944-1');
+          `}
+        </Script>
       </Head>
       <body className="bg-bright-bg text-dark-lightest dark:bg-dark dark:text-white">
         <Main />
