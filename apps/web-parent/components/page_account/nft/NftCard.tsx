e68439ac6ab4c5ab64d3a07fbe54796cbddeba44
tipii <theo.palhol@gmail.com>
2022-07-17 02:42:58 +0200

    feat(web-parent): add modal to nftCard

--- apps/web-parent/components/page_account/nft/NftCard.tsx
+++ apps/web-parent/components/page_account/nft/NftCard.tsx
@@ -1,7 +1,9 @@
 import { OwnedNft } from '@alch/alchemy-sdk';
-import Link from 'next/link';
+import { useState } from 'react';
+import NftDialog from './NftDialog';
 
 function NftCard({ nft }: { nft: OwnedNft }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="container-classic col-span-4 flex flex-col overflow-hidden rounded-md">
       {nft.media[0].gateway && (
@@ -13,12 +15,13 @@ function NftCard({ nft }: { nft: OwnedNft }) {
       )}
       <div className="flex flex-grow flex-col justify-between p-2">
         <div>
-          <h3 className="font-bold">{nft.title}</h3>
+          <h3 className="">{nft.title}</h3>
         </div>
-        <Link href="#">
-          <a className="mt-2 text-right text-primary">See more</a>
-        </Link>
+        <a className="text-right" onClick={() => setIsOpen(true)}>
+          See more
+        </a>
       </div>
+      <NftDialog isOpen={isOpen} setIsOpem={setIsOpen}></NftDialog>
     </div>
   );
 }
