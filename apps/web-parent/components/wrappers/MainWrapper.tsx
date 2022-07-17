c3bdbecb48592a3d4288d33bb96580ed2036d6f9
tipii <theo.palhol@gmail.com>
2022-07-17 02:46:09 +0200

    refactor(web-parent): fix section container and main wrapper

--- apps/web-parent/components/wrappers/MainWrapper.tsx
+++ apps/web-parent/components/wrappers/MainWrapper.tsx
@@ -1,3 +1,4 @@
+import BackgoundSplit from '@lib/ui/src/Background/BackgoundSplit';
 import Header from '../header/Header';
 import AuthGuard from './AuthGuard';
 
@@ -14,17 +15,21 @@ function MainWrapper({
 }: MainWrapperProps) {
   if (!authProtected) {
     return (
-      <div className="max-w-screen min-h-screen overflow-hidden">
+      <div className="relative min-h-screen overflow-hidden px-4">
         {!noHeader && <Header />}
         {children}
+        <BackgoundSplit className="-top-80 -left-96" />
+        <BackgoundSplit className="-bottom-[50vh] -right-96" />
       </div>
     );
   }
 
   return (
-    <div className="max-w-screen min-h-screen overflow-hidden">
+    <div className="relative min-h-screen overflow-hidden px-4">
       {!noHeader && <Header />}
       <AuthGuard>{children}</AuthGuard>
+      <BackgoundSplit className="-top-80 -left-96" />
+      <BackgoundSplit className="-bottom-[50vh] -right-96" />
     </div>
   );
 }
