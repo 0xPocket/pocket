0f586e497c30be3c83ca737d2ab7a2a9d1911c80
Solal Dunckel <solaldunckel@gmail.com>
2022-07-18 10:03:44 +0200

    feat(web-parent): add Ramp history to the database and webhook

--- apps/api/src/ramp/ramp.module.ts
+++ apps/api/src/ramp/ramp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RampService } from './ramp.service';
 import { RampController } from './ramp.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RampController],
   providers: [RampService],
 })
