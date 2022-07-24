d9fe9ac1f5701c5f3e3e7114f03f550667a19fe7
Solal Dunckel <solaldunckel@gmail.com>
2022-07-24 19:36:44 +0200

    feat(web-parent/wagmi): add hooks for typesafe contract call with `wagmi`

---
+++ apps/web-parent/hooks/useContractWrite.ts
@@ -0,0 +1,52 @@
+import type { TransactionResponse } from '@ethersproject/providers';
+import type { CallOverrides, Contract } from 'ethers';
+import { useContractWrite as wagmiUseContractWrite } from 'wagmi';
+import type { UseContractWriteArgs } from 'wagmi/dist/declarations/src/hooks/contracts/useContractWrite';
+
+type ContractMethodNames<T extends Contract> = keyof T['functions'];
+
+type ContractMethodArgs<
+  T extends Contract,
+  U extends ContractMethodNames<T>,
+> = T['functions'][U] extends (...args: infer P) => unknown ? P : never;
+
+type UseContractWriteParams<
+  T extends Contract,
+  U extends ContractMethodNames<T>,
+> = Omit<UseContractWriteArgs, 'addressOrName' | 'contractInterface'> & {
+  contract: T;
+  functionName: U;
+  args?: ContractMethodArgs<T, U>;
+};
+
+type UseContractWriteMutationArgs<
+  T extends Contract,
+  U extends ContractMethodNames<T>,
+> = {
+  args: ContractMethodArgs<T, U>;
+  overrides?: CallOverrides;
+};
+
+export type UseContractWriteReturn<
+  T extends Contract,
+  U extends ContractMethodNames<T>,
+> = ReturnType<typeof wagmiUseContractWrite> & {
+  write: (
+    overrideConfig?: UseContractWriteMutationArgs<T, U> | undefined,
+  ) => void;
+  writeAsync: (
+    overrideConfig?: UseContractWriteMutationArgs<T, U> | undefined,
+  ) => Promise<TransactionResponse>;
+};
+
+export default function useContractWrite<
+  T extends Contract,
+  U extends ContractMethodNames<T>,
+>(params: UseContractWriteParams<T, U>): UseContractWriteReturn<T, U> {
+  const { contract, ...rest } = params;
+  return wagmiUseContractWrite({
+    addressOrName: contract.address,
+    contractInterface: contract.interface,
+    ...rest,
+  }) as UseContractWriteReturn<T, U>;
+}
