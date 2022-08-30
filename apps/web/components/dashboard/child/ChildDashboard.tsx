83a4d4f54e03cf6125c950841b7d0c8c79c29a1e
tipii <theo.palhol@gmail.com>
2022-08-30 13:26:07 +0200

    style(web): add components to child dashboard

--- apps/web/components/dashboard/child/ChildDashboard.tsx
+++ apps/web/components/dashboard/child/ChildDashboard.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useAccount } from 'wagmi';
-import { useSmartContract } from '../../../contexts/contract';
 import NftContent from '../common/nft/NftContent';
 import TokenContent from '../common/token/TokenContent';
 import ActivityContent from '../common/activity/ActivityContent';
 import ChildCard from '../../card/child/ChildCard';
-import Swapper from './Swapper';
+import dynamic from 'next/dynamic';
+import { SwapWidgetProps } from '@uniswap/widgets';
 
+const SwapWidget = dynamic<SwapWidgetProps>(
+  () => import('@uniswap/widgets').then((mod) => mod.SwapWidget),
+  { ssr: false },
+);
 const ChildDashboard: React.FC = () => {
   const { address } = useAccount();
 
@@ -14,14 +18,13 @@ const ChildDashboard: React.FC = () => {
     <div className="space-y-20">
       <div className="grid grid-cols-2 gap-8">
         <ChildCard childAddress={address} className="col-span-1" />
-        <Swapper />
-        {/* <ChildCard childAddress={address} className="col-span-1" /> */}
+        <SwapWidget />
       </div>
       <div className="grid grid-cols-2 gap-8">
-        {/* <TokenContent childAddress={address!} /> */}
-        {/* <NftContent childAddress={address!} fill_nbr={9} /> */}
+        <NftContent childAddress={address!} fill_nbr={9} />
         <ActivityContent childAddress={address!} userType="Child" />
       </div>
+      <TokenContent childAddress={address!} />
     </div>
   ) : (
     <div></div>
