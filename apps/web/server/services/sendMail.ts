f69fd80f3902a6282c1f5112ea43a2f489aa274c
Solal Dunckel <solaldunckel@gmail.com>
2022-08-30 14:18:17 +0200

    feat(web): add template id for `child_invitation`

--- apps/web/server/services/sendMail.ts
+++ apps/web/server/services/sendMail.ts
@@ -6,7 +6,7 @@ sgMail.setApiKey(env.SENDGRID_API_KEY);
 
 const SENDGRID_EMAIL_TEMPLATES = {
   email_verification: 'd-801f7f84d63f46d08f5f89e9fa85ca09',
-  child_invitation: 'd-801f7f84d63f46d08f5f89e9fa85ca09',
+  child_invitation: 'd-230f8a8c70e94752923780db5775e57f',
 };
 
 type SendgridTemplateKeys = keyof typeof SENDGRID_EMAIL_TEMPLATES;
