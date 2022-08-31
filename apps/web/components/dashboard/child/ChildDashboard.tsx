97cbf29808c6dc174fa785b499e00baf4c2ba33d
tipii <theo.palhol@gmail.com>
2022-08-31 15:10:15 +0200

    fix(web): rm unused lib / unused deps

--- apps/web/components/dashboard/child/ChildDashboard.tsx
+++ apps/web/components/dashboard/child/ChildDashboard.tsx
@@ -4,9 +4,6 @@ import NftContent from '../common/nft/NftContent';
 import TokenContent from '../common/token/TokenContent';
 import ActivityContent from '../common/activity/ActivityContent';
 import ChildCard from '../../card/child/ChildCard';
-import dynamic from 'next/dynamic';
-import { SwapWidgetProps, Theme } from '@uniswap/widgets';
-import { useTheme } from '@lib/ui/src/Theme/ThemeContext';
 import Swapper from './Swapper';
 
 const ChildDashboard: React.FC = () => {
