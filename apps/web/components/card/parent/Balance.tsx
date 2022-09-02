a32c5cbdeb2e2299542db15e4fd58119fd2c8af4
Solal Dunckel <solaldunckel@gmail.com>
2022-09-02 16:52:05 +0200

    chore: remove console.log

--- apps/web/components/card/parent/Balance.tsx
+++ apps/web/components/card/parent/Balance.tsx
@@ -13,8 +13,7 @@ type BalanceProps = {
 
 function Balance({ value, setSelectedIndex, hideActions }: BalanceProps) {
   const { erc20 } = useSmartContract();
-  console.log(value);
-  console.log(erc20.data?.decimals);
+
   return (
     <>
       <div className="flex flex-col items-end">
