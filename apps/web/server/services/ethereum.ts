aef2cee2915c7f7155c1c8907a7fe8d74a045b20
Solal Dunckel <solaldunckel@gmail.com>
2022-09-07 11:44:54 +0200

    fix(web/api): reduce grant amount for test-net values

--- apps/web/server/services/ethereum.ts
+++ apps/web/server/services/ethereum.ts
@@ -13,8 +13,8 @@ const pocketContract = PocketFaucet__factory.connect(
   provider,
 );
 
-const AMOUNT_TO_SEND_TO_CHILD = parseEther('20');
-const AMOUNT_TO_SEND_TO_PARENT = parseEther('10');
+const AMOUNT_TO_SEND_TO_CHILD = parseEther('0.2');
+const AMOUNT_TO_SEND_TO_PARENT = parseEther('0.1');
 const AMOUNT_OF_GRANTS = 5;
 
 async function getPriorityFeeData() {
