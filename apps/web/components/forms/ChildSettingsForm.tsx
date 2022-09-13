cc511c3dcdbc6b64a6b2fe7ffc64849e694e1754
tipii <theo.palhol@gmail.com>
2022-09-13 05:06:38 +0200

    style(web): child setting style, implement radio group, style input number

--- apps/web/components/forms/ChildSettingsForm.tsx
+++ apps/web/components/forms/ChildSettingsForm.tsx
@@ -1,8 +1,9 @@
 import { faAngleLeft, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FormErrorMessage } from '@lib/ui';
+import { RadioGroup } from '@headlessui/react';
 import { formatUnits } from 'ethers/lib/utils';
 import { PocketFaucet } from 'pocket-contract/typechain-types';
+import { useEffect, useState } from 'react';
 
 import { z } from 'zod';
 import { useSmartContract } from '../../contexts/contract';
@@ -14,6 +15,10 @@ const ChildSettingsSchema = z.object({
   ceiling: z.number({ invalid_type_error: 'Ceiling is required' }).min(1),
   periodicity: z.string(),
 });
+const periodicity_options = [
+  { name: 'weekly', value: '604800' },
+  { name: 'monthly', value: '2592000' },
+];
 
 type FormValues = z.infer<typeof ChildSettingsSchema>;
 
@@ -30,11 +35,19 @@ function ChildSettingsForm({
   changeConfig,
   returnFn,
 }: ChildSettingsFormProps) {
+  const [selected, setSelected] = useState(
+    periodicity_options.find(
+      (option) =>
+        option.value === formatUnits(config.periodicity, 0).toString(),
+    ) ?? periodicity_options[0],
+  );
+
   const { erc20 } = useSmartContract();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useZodForm({
     schema: ChildSettingsSchema,
     defaultValues: {
@@ -48,49 +61,58 @@ function ChildSettingsForm({
     changeConfig(data);
   };
 
+  useEffect(() => {
+    setValue('periodicity', selected.value);
+  }, [selected, setValue]);
+
   return (
     <>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex h-full flex-col items-end justify-between space-y-4"
       >
-        <div className="flex items-center space-x-8">
+        <RadioGroup
+          value={selected}
+          onChange={setSelected}
+          className="flex items-center space-x-8"
+        >
           <label>Periodicity</label>
-          <div className="flex flex-col space-x-2">
-            <input
-              type="radio"
-              id="weekly"
-              value="604800"
-              {...register('periodicity')}
-            />
-            <label htmlFor="huey">Weekly</label>
-          </div>
 
-          <div className="flex flex-col space-x-2">
-            <input
-              type="radio"
-              id="monthly"
-              value="2592000"
-              {...register('periodicity')}
-            />
-            <label htmlFor="dewey">Monthly</label>
-          </div>
-        </div>
+          {periodicity_options.map((option) => (
+            <RadioGroup.Option
+              key={option.name}
+              value={option}
+              className={({ checked }) =>
+                checked ? 'input-radio-checked' : 'input-radio-unchecked'
+              }
+            >
+              {option.name}
+            </RadioGroup.Option>
+          ))}
+        </RadioGroup>
+
         <div className="flex items-center space-x-8">
           <label htmlFor="topup">Ceiling</label>
-          <input
-            className="border p-2 text-dark"
-            {...register('ceiling', {
-              valueAsNumber: true,
-            })}
-            type="number"
-          />
-          {errors.ceiling && (
-            <FormErrorMessage message={errors.ceiling.message} />
-          )}
+          <div className="relative flex  items-center text-4xl">
+            <input
+              className="input-number"
+              placeholder="0"
+              type="number"
+              min="0"
+              {...register('ceiling', {
+                valueAsNumber: true,
+              })}
+            />
+            <span>$</span>
+            {/* {errors.ceiling && (
+              <span className="absolute bottom-0 right-0 translate-y-full rounded border border-danger bg-danger/20 p-1 px-2 text-xs text-white">
+                {errors.ceiling.message}
+              </span>
+            )} */}
+          </div>
         </div>
 
-        <button
+        {/* <button
           className="third-btn mt-4"
           onClick={(e) => {
             e.preventDefault();
@@ -98,7 +120,7 @@ function ChildSettingsForm({
           }}
         >
           withdraw funds
-        </button>
+        </button> */}
 
         <div className="flex space-x-4">
           <button
