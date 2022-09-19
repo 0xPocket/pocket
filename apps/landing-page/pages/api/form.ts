cb71ad171818f0852f75d43cb202d014a4129e6a
Solal Dunckel <solaldunckel@gmail.com>
2022-09-19 10:21:07 +0200

    feat(landing-page): add invisible reCAPTCHA validation on landing-page's CTA

--- apps/landing-page/pages/api/form.ts
+++ apps/landing-page/pages/api/form.ts
@@ -2,24 +2,57 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import { formSchema } from '../survey';
 import { prisma } from '../../lib/prisma';
+import { env } from 'config/env/server';
 
 type Data = z.infer<typeof formSchema>;
+
+type reCaptchaResponse = {
+  success: boolean;
+  challenge_ts: string;
+  hostname: string;
+};
+
+async function validateCaptcha(captcha: string | null | undefined) {
+  if (!captcha) {
+    return false;
+  }
+  return fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      },
+      method: 'POST',
+    },
+  ).then((response) =>
+    (response.json() as Promise<reCaptchaResponse>).then(
+      (data) => data.success,
+    ),
+  );
+}
 
 const form = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const data = req.body as Data;
     try {
-      console.log(data);
       const validation = formSchema.parse(data);
+      const captchaValidation = await validateCaptcha(data.captcha);
+
+      if (!captchaValidation) {
+        return res.status(400).json({ error: 'Invalid captcha' });
+      }
+
       await prisma.survey.upsert({
         where: {
           email: validation.email,
         },
         create: {
-          ...validation,
+          email: validation.email,
+          contact: validation.contact,
         },
-        update: { ...validation },
+        update: { email: validation.email, contact: validation.contact },
       });
+
       return res.status(201).json({ message: 'Success !' });
     } catch (e) {
       console.log(e);
