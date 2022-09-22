289fc73cbc0b360603b9eb44d2eafffd9a4bc953
Solal Dunckel <solaldunckel@gmail.com>
2022-09-22 11:56:39 +0200

    feat(web): make account page client-side only

--- apps/web/pages/account/[id].tsx
+++ apps/web/pages/account/[id].tsx
@@ -1,34 +1,25 @@
 import MainWrapper from '../../components/common/wrappers/MainWrapper';
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from 'next';
 import AccountDashboard from '../../components/dashboard/parent/Dashboard';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { trpc } from '../../utils/trpc';
 import FormattedMessage from '../../components/common/FormattedMessage';
+import { useRouter } from 'next/router';
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const id = context.params?.id as string;
+function Account() {
+  const router = useRouter();
+  const id = router.query.id as string;
 
-  return {
-    props: {
-      address: id,
+  const { isLoading, data: child } = trpc.useQuery(
+    ['parent.childByAddress', { address: id }],
+    {
+      enabled: !!id,
     },
-  };
-}
-
-function Account({
-  address,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { isLoading, data: child } = trpc.useQuery([
-    'parent.childByAddress',
-    { address },
-  ]);
+  );
 
   if (!child) return null;
+
   return (
     <MainWrapper>
       <div className="mb-12 flex items-center space-x-4">
