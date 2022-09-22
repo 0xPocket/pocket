2ed75551ecadd4845f6d4317f1a168343b0f0093
Solal Dunckel <solaldunckel@gmail.com>
2022-09-22 10:57:00 +0200

    feat(landing-page): add bundle-analyzer

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -2,8 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: '../../.env' });
 
-const withTM = require('next-transpile-modules')([]);
-
 if (
   !process.env.NEXT_PUBLIC_RECAPTCHA_PUBLIC_KEY ||
   !process.env.RECAPTCHA_SECRET_KEY
@@ -11,16 +9,21 @@ if (
   throw new Error('Missing reCAPTCHA keys');
 }
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     locales: ['en-US', 'fr'],
     defaultLocale: 'en-US',
   },
+  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['pocket-eu.s3.eu-west-3.amazonaws.com'],
   },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = withBundleAnalyzer(nextConfig);
