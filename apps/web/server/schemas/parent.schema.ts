c34a5e6f3d170da56bc2851e20534302b80070b5
tipii <theo.palhol@gmail.com>
2022-09-26 18:24:54 +0200

    fix(web): limit child name size

--- apps/web/server/schemas/parent.schema.ts
+++ apps/web/server/schemas/parent.schema.ts
@@ -2,7 +2,10 @@ import { z } from 'zod';
 
 export const ParentSchema = {
   createChild: z.object({
-    name: z.string().min(1, 'Name cannot be empty'),
+    name: z
+      .string()
+      .min(1, 'Name cannot be empty')
+      .max(15, 'Name cannot be longer than 15 characters'),
     email: z.string().email('Email is invalid'),
     ceiling: z.number({ invalid_type_error: 'Ceiling is required' }).min(1),
     periodicity: z.string(),
