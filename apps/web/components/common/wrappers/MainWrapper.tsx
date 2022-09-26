72258939410d06ad9fa120112c420ab618835c53
tipii <theo.palhol@gmail.com>
2022-09-26 18:21:11 +0200

    refactor(web): better wrapper

--- apps/web/components/common/wrappers/MainWrapper.tsx
+++ apps/web/components/common/wrappers/MainWrapper.tsx
@@ -16,21 +16,14 @@ const MainWrapper: FC<MainWrapperProps> = ({
   noFooter = false,
   noContainer = false,
 }) => {
-  if (noContainer)
-    return (
-      <div className="relative min-h-screen overflow-hidden px-4 pb-20">
-        {!noHeader && <Header />}
-        {children}
-        <BackgoundSplit className="-top-80 -left-96" />
-        <BackgoundSplit className="-bottom-[50vh] -right-96" />
-        {!noFooter && <Footer className="absolute bottom-0 left-0 right-0" />}
-      </div>
-    );
-
   return (
     <div className="relative min-h-screen overflow-hidden px-4 pb-20">
       {!noHeader && <Header />}
-      <div className="container relative mx-auto my-12">{children}</div>
+      {!noContainer ? (
+        <div className="container relative mx-auto my-12">{children}</div>
+      ) : (
+        children
+      )}
       <BackgoundSplit className="-top-80 -left-96" />
       <BackgoundSplit className="-bottom-[50vh] -right-96" />
       {!noFooter && <Footer className="absolute bottom-0 left-0 right-0" />}
