bd9122103e0dd6e1afa393ab9c993984ea56b819
tipii <theo.palhol@gmail.com>
2022-10-05 13:41:29 +0200

    fix(web): display lang and theme toggler when not logged in

--- apps/web/components/header/Header.tsx
+++ apps/web/components/header/Header.tsx
@@ -11,12 +11,10 @@ function GlobalHeader() {
       <Header.BlockLeft>
         <Header.Title href="/">Pocket.</Header.Title>
       </Header.BlockLeft>
-      {loggedIn && (
-        <Header.BlockRight>
-          <WalletPopover />
-          <DropdownMenu />
-        </Header.BlockRight>
-      )}
+      <Header.BlockRight>
+        {loggedIn && <WalletPopover />}
+        <DropdownMenu />
+      </Header.BlockRight>
     </Header>
   );
 }
