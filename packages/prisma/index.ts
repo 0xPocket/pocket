8a7649fce69c5e7b089746cc8225a45e7543d035
Solal Dunckel <solaldunckel@gmail.com>
2022-11-09 09:22:49 +0100

    feat(prisma): export a client from prisma package

---
+++ packages/prisma/index.ts
@@ -0,0 +1,19 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaOptions: Prisma.PrismaClientOptions = {};
+
+if (!!process.env.NEXT_PUBLIC_DEBUG)
+  prismaOptions.log = ["query", "error", "warn"];
+
+export const prisma = global.prisma || new PrismaClient(prismaOptions);
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
