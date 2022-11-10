aecc97060bb72d0cd3ab79139c3b6bb2884e5b2f
Solal Dunckel <solaldunckel@gmail.com>
2022-11-10 16:14:43 +0100

    fix(web): fix connection on mobile

--- apps/web/components/connect/ConnectProviderList.tsx
+++ apps/web/components/connect/ConnectProviderList.tsx
@@ -1,5 +1,5 @@
 import { type FC, useCallback, useState } from 'react';
-import { Connector, useAccount, useConnect } from 'wagmi';
+import { Connector, useAccount, useConnect, useDisconnect } from 'wagmi';
 import Image from 'next/future/image';
 import { DialogPopupWrapper } from '../common/wrappers/DialogsWrapper';
 import EmailModalForm from './EmailModalForm';
@@ -9,7 +9,7 @@ import { Spinner } from '../common/Spinner';
 import { useIsMounted } from '../../hooks/useIsMounted';
 
 const ProviderList: FC = () => {
-  const { connector: activeConnector, status } = useAccount();
+  const { connector: activeConnector, status, isConnected } = useAccount();
   const isMounted = useIsMounted();
   const ethereumSign = useEthereumSiwe({
     onSuccess: ({ message, signature }) => {
@@ -21,13 +21,11 @@ const ProviderList: FC = () => {
     },
   });
 
+  const { disconnect } = useDisconnect();
+
   const { signIn, isLoading } = useSignIn();
 
-  const { connectors, connect } = useConnect({
-    onSuccess: async () => {
-      ethereumSign.mutate();
-    },
-  });
+  const { connectors, connect } = useConnect();
   const [open, setIsOpen] = useState(false);
 
   const handleConnect = useCallback(
@@ -50,28 +48,40 @@ const ProviderList: FC = () => {
       {isLoading ||
       !isMounted ||
       status === 'connecting' ||
+      open ||
       ethereumSign.isLoading ? (
         <Spinner />
+      ) : isConnected ? (
+        <>
+          <button className="action-btn" onClick={() => ethereumSign.mutate()}>
+            Sign in with ethereum
+          </button>
+          <button className="third-btn py-0" onClick={() => disconnect()}>
+            go back
+          </button>
+        </>
       ) : (
         <div className={` flex w-full justify-center gap-8`}>
-          {connectors.map((connector) => (
-            <button
-              key={connector.id}
-              onClick={() => handleConnect(connector)}
-              className="provider-container disabled:opacity-50"
-            >
-              <div className="container-classic rounded-md p-8">
-                <div className="provider-img">
-                  <Image
-                    src={`/assets/providers/${connector.id}.svg`}
-                    width={128}
-                    height={128}
-                    alt={connector.name}
-                  />
+          {connectors
+            .filter((x) => x.ready)
+            .map((connector) => (
+              <button
+                key={connector.id}
+                onClick={() => handleConnect(connector)}
+                className="provider-container disabled:opacity-50"
+              >
+                <div className="container-classic rounded-md p-8">
+                  <div className="provider-img">
+                    <Image
+                      src={`/assets/providers/${connector.id}.svg`}
+                      width={128}
+                      height={128}
+                      alt={connector.name}
+                    />
+                  </div>
                 </div>
-              </div>
-            </button>
-          ))}
+              </button>
+            ))}
         </div>
       )}
 
