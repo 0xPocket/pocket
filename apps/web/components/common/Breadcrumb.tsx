c5f4b97c3ea7351bb679affbf47276cbe0d92604
tipii <theo.palhol@gmail.com>
2022-11-18 10:49:38 +0100

    fix(web): fix breadcrumb hydratation problem

--- apps/web/components/common/Breadcrumb.tsx
+++ apps/web/components/common/Breadcrumb.tsx
@@ -18,63 +18,52 @@ type BreadcrumbProps = {
   routes: route[];
 };
 
-// isMobile hook
-
-function useIsMobile() {
-  return typeof window !== 'undefined' && window.innerWidth < 768;
-}
-
 const Breadcrumb: FC<BreadcrumbProps> = ({ routes }) => {
-  const isMobile = useIsMobile();
   const router = useRouter();
 
   const splittedPathname = router.asPath.split('/').filter(Boolean);
 
-  if (isMobile) {
-    if (splittedPathname.length === 0) {
-      return null;
-    }
-    return (
-      <div className="mb-12 flex items-center space-x-4">
-        <button onClick={() => router.back()} className="text-2xl">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-      </div>
-    );
-  }
-
   return (
-    <div className="mb-12 flex items-center space-x-4">
-      {routes.length === 0 ? (
-        <p>
-          <FontAwesomeIcon icon={faHome} className="mr-2" /> Home
-        </p>
-      ) : (
-        <>
-          <Link href="/">
-            <a>
-              <FontAwesomeIcon icon={faHome} className="mr-2" /> Home
-            </a>
-          </Link>
-          <FontAwesomeIcon icon={faAngleRight} />
-        </>
-      )}
-
-      {routes.map((route, i) => (
-        <div key={i} className="flex items-center">
-          {route.path ? (
-            <Link href={route.path}>
-              <a>{route.name}</a>
-            </Link>
-          ) : (
-            <p>{route.name}</p>
-          )}
-          {i !== routes.length - 1 && (
-            <FontAwesomeIcon icon={faAngleRight} className="ml-4" />
-          )}
+    <>
+      {splittedPathname.length !== 0 && (
+        <div className="mb-12 flex items-center space-x-4 md:hidden">
+          <button onClick={() => router.back()} className="text-2xl">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
         </div>
-      ))}
-    </div>
+      )}
+      <div className="mb-12 hidden items-center space-x-4 md:flex">
+        {routes.length === 0 ? (
+          <p>
+            <FontAwesomeIcon icon={faHome} className="mr-2" /> Home
+          </p>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <FontAwesomeIcon icon={faHome} className="mr-2" /> Home
+              </a>
+            </Link>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </>
+        )}
+
+        {routes.map((route, i) => (
+          <div key={i} className="flex items-center">
+            {route.path ? (
+              <Link href={route.path}>
+                <a>{route.name}</a>
+              </Link>
+            ) : (
+              <p>{route.name}</p>
+            )}
+            {i !== routes.length - 1 && (
+              <FontAwesomeIcon icon={faAngleRight} className="ml-4" />
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
