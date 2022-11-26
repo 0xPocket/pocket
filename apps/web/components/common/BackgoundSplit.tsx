8903ef98c993dec2d00c9cfc2e77ead957356fbb
Solal Dunckel <solaldunckel@gmail.com>
2022-11-26 18:02:50 +0100

    fix(web): fix system light mode style

--- apps/web/components/common/BackgoundSplit.tsx
+++ apps/web/components/common/BackgoundSplit.tsx
@@ -5,7 +5,7 @@ type BackgoundSplitProps = {
 function BackgoundSplit({ className }: BackgoundSplitProps) {
   return (
     <div
-      className={`${className} absolute -z-50 h-[1080px] w-[1920px] bg-dark-radial-herosection dark:opacity-10`}
+      className={`${className} absolute -z-50 h-[1080px] w-[1920px] bg-dark-radial-herosection opacity-10`}
     ></div>
   );
 }
